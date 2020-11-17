b09df36ae22e32dc760acb1c269a24dadebe47bd
Clint Mann <climann2@cisco.com>
2020-11-17 10:46:30 -0500

    updating main

--- main.py
+++ main.py
@@ -38,4 +38,5 @@ def postAlerts(data):
     return 'complete'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run()
+    # app.run(host='0.0.0.0', port=5000, debug=False)
